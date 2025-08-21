faaa680564a1bcffb5f96588736e952086ec5667
jasirpanghat <jazjasir7@gmail.com>
2025-08-21 22:38:12 +0530

    feat: create django apps which are users,chat and create user entity

---
+++ backend/apps/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomeUser (AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
