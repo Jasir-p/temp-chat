55d687dc77bcfc856efef2903f7a28cf580cae37
jasirpanghat <jazjasir7@gmail.com>
2025-08-30 17:44:35 +0530

    Added chatpage componnet for after join a room and websocket hook for realtime chat connection

---
+++ frontend/temp_chat/src/hooks/useWebsocket.jsx
@@ -0,0 +1,54 @@
+import { useEffect,useRef,useCallback } from "react";
+
+
+export const useChatSocket = (roomID,setMessage,setOnlineMembers)=>{
+    const socketRef = useRef(null)
+    const WS_URL = import.meta.env.VITE_WS_URL;
+
+    useEffect(()=>{
+        const socket = new WebSocket(
+            `${WS_URL}/ws/chat/${roomID}/`
+        )
+        socketRef.current = socket
+
+
+        socket.onopen = ()=>{
+            console.log("connected");
+            
+        }
+        socket.onerror = (error)=>{
+            console.error("webSocket error", error);
+            
+        }
+        socket.onclose = (event) =>{
+            console.log("websocket closed",event);
+            
+        }
+        socket.onmessage = (event) => {
+            console.log("message from server:", event.data);
+            const eventdata = JSON.parse(event.data)
+            if (eventdata.type==="chat"){
+                setMessage((prev)=>[...prev,eventdata.message])
+            }
+            if (eventdata.type==="members"){
+                setOnlineMembers(eventdata.members)
+            }
+            
+
+            }
+
+        return () => {
+      socket.close();
+    };
+    },[roomID])
+    const sendMessage = useCallback((message) => {
+        if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN){
+            socketRef.current.send(JSON.stringify({message}));
+        } else{
+            console.error("WebSocket is not connected.")
+        }
+    },[]);
+
+    return { sendMessage,socket:socketRef.current };
+    
+}
---
+++ frontend/temp_chat/src/pages/ChatPage.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect,useState } from 'react'
+import { useChatSocket } from '../hooks/useWebsocket'
+import { ChatHeader } from '../components/chatpage/ChatHeader'
+import MainLayout from '../layouts/MainLayout'
+import { Message } from '../components/chatpage/Message'
+import { ChatInput } from '../components/chatpage/ChatInput'
+import { Sidebar } from '../components/chatpage/Sidebar'
+import { fetchMessage } from '../api/ChatMessageApi'
+import { getSingleChatRoom } from '../api/ChatRoomApi'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+
+
+const ChatPage = () => {
+    
+const {roomId} = useParams()
+const[messages,setMessages]= useState([])
+const [onlineMembers,setOnlineMembers]= useState([])
+const {sendMessage,socket} = useChatSocket(roomId,setMessages,setOnlineMembers)
+const [sidebarOpen, setSidebarOpen] = useState(false);
+const userId = useSelector((state)=>state.auth.userId)
+const [roomData,setRoomData] = useState(null)
+
+
+useEffect (()=>{
+    if (!socket) return
+},[socket])
+
+console.log(roomData);
+
+useEffect (()=>{
+  const getMessages = async()=>{
+    try{
+    const data = await fetchMessage(roomId)
+    setMessages(data)
+  } catch(error){
+    console.log(error);
+    
+  }
+}
+if (roomId){
+  getMessages()
+}
+
+},[roomId])
+
+
+useEffect (()=>{
+  const getChatRoom = async()=>{
+    try{
+      const data = await getSingleChatRoom(roomId)
+      setRoomData(data)
+    }catch(error){
+      console.log(error);
+      
+    }
+
+  }
+  if(roomId){
+    getChatRoom()
+  }
+},[roomId])
+console.log(onlineMembers);
+
+
+  return (
+    <MainLayout>
+      <div className={`flex ${sidebarOpen ? 'w-[1200px]' : 'w-[900px]'} h-[600px] bg-gray-50 shadow-md mx-auto my-10 transition-all duration-300`}>
+        
+        {/* Main Chat Area */}
+        <div className={`flex flex-col ${sidebarOpen ? 'w-2/3' : 'w-full'} transition-all duration-300`}>
+          <ChatHeader roomName={roomData?.name} onViewAbout={() => setSidebarOpen(true)} onlineCount={onlineMembers.length} />
+
+          <div className='flex-1 overflow-y-auto px-6 py-4'>
+            {messages.map((message) => (
+              <Message 
+                key={message.id} 
+                message={message}
+                isOwn={message.user.id ===userId} 
+              />
+            ))}
+          </div>
+          
+          <ChatInput onSend ={(msg)=>sendMessage(msg)} />
+        </div>
+
+        {/* Sidebar */}
+        {sidebarOpen && (
+          <div className="w-1/3 animate-slide-in">
+            <Sidebar 
+              isOpen={sidebarOpen} 
+              onClose={() => setSidebarOpen(false)}
+              roomData={roomData}
+              onlineCount={onlineMembers.length}
+              members={onlineMembers}
+              
+            />
+          </div>
+        )}
+        
+      </div>
+    </MainLayout>
+    
+  )
+}
+
+export default ChatPage
