f8048c230505ea077bd2c04fcc5210224630b969
jasirpanghat <jazjasir7@gmail.com>
2025-08-30 23:08:37 +0530

    modified settings.py with .env file and add package into requirements.txt

--- backend/temp_chat/settings.py
+++ backend/temp_chat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
@@ -68,9 +68,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ALLOWED_ORIGINS = [
-  "http://localhost:5173",  
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", default="").split(",")
 
 CORS_ALLOW_CREDENTIALS = True
 SIMPLE_JWT = {
