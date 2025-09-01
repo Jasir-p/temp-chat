5f5fbd51a97342c4f94864abb4d015c42a5d5c91
jasirpanghat <jazjasir7@gmail.com>
2025-09-01 13:26:52 +0530

    modified views part in userprofile and set jwt section settings.py

--- backend/temp_chat/settings.py
+++ backend/temp_chat/settings.py
@@ -81,7 +81,7 @@ SIMPLE_JWT = {
     'AUTH_COOKIE_REFRESH': 'refresh_token', 
     "AUTH_COOKIE_SECURE": True,  # restricts the transmission of the cookie to only occur over secure (HTTPS) connections. 
     "AUTH_COOKIE_HTTP_ONLY": True,  # prevents client-side js from accessing the cookie
-    "AUTH_COOKIE_SAMESITE": None,  # specifies whether the cookie should be sent in cross site requests
+    "AUTH_COOKIE_SAMESITE": "None",  # specifies whether the cookie should be sent in cross site requests
 }
 
 
