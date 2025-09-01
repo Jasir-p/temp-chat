d55ccd0bfc0428db2bcf8a906ae726e754441560
jasirpanghat <jazjasir7@gmail.com>
2025-09-01 15:18:17 +0530

    fix: logout 500 error issue and modified settings.py

--- backend/temp_chat/settings.py
+++ backend/temp_chat/settings.py
@@ -75,8 +75,8 @@ CORS_ALLOW_CREDENTIALS = True
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_COOKIE": "access_token",
     'AUTH_COOKIE_REFRESH': 'refresh_token', 
